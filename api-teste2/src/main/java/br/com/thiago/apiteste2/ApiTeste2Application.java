e26e3c1eb68dfe2de4fcf2653b6954107c2a9803
Thiago <tlucenalive@gmail.com>
2024-04-02 14:24:13 -0300

    configuração do cliente 1 application.properties

--- api-teste2/src/main/java/br/com/thiago/apiteste2/ApiTeste2Application.java
+++ api-teste2/src/main/java/br/com/thiago/apiteste2/ApiTeste2Application.java
@@ -2,6 +2,9 @@ package br.com.thiago.apiteste2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiTeste2Application {
@@ -9,5 +12,16 @@ public class ApiTeste2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTeste2Application.class, args);
 	}
+
+}
+
+@RestController
+@RequestMapping(path = "/cliente2")
+class ClienteDois {
+    @GetMapping
+    public String Oladois(){
+        return "Eu sou o cliente 2";
+    }
+
 
 }
