aaa2aa9f38da7966511573a84d41d18370e9b944
Thiago <tlucenalive@gmail.com>
2024-04-02 11:06:29 -0300

    Configuracao do Servico Eureka adicionando annotation e aplicattions.properties

---
+++ api-teste1/src/main/java/br/com/thiago/apiteste1/ApiTeste1Application.java
@@ -0,0 +1,13 @@
+package br.com.thiago.apiteste1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTeste1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTeste1Application.class, args);
+	}
+
+}
---
+++ api-teste1/src/test/java/br/com/thiago/apiteste1/ApiTeste1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.apiteste1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTeste1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
